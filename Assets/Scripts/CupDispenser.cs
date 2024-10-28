f54919213aeda8fafc77bc9da58bcb77b2b5fcd3
refigo <refi@xyzcorp.io>
2024-10-28 18:04:33 +0900

    Feat: Set CupDispenser as CafeObjectParent

--- Assets/Scripts/CupDispenser.cs
+++ Assets/Scripts/CupDispenser.cs
@@ -1,18 +1,41 @@
 using UnityEngine;
 
-public class CupDispenser : MonoBehaviour {
+public class CupDispenser : MonoBehaviour, ICafeObjectParent {
 
     // [SerializeField] private Transform coffeeCupPrefab;
     [SerializeField] private CafeObjectSO cafeObjectSO;
     [SerializeField] private Transform dispenserFrontPoint;
 
-    private CafeObject cafeObject;
-    
-    public void Interact() {
-        Debug.Log("Interact!");
-        Transform cafeObjectTransform = Instantiate(cafeObjectSO.prefab, dispenserFrontPoint);
-        cafeObjectTransform.localPosition = Vector3.zero;
 
-        Debug.Log(cafeObjectTransform.GetComponent<CafeObject>().GetCafeObjectSO().objectName);
+    private CafeObject cafeObject;
+
+
+    public void Interact() {
+        if (cafeObject == null) {
+            Transform cafeObjectTransform = Instantiate(cafeObjectSO.prefab, dispenserFrontPoint);
+            cafeObjectTransform.GetComponent<CafeObject>().SetCafeObjectParent(this);
+        } else {
+            Debug.Log(cafeObject.GetCafeObjectParent());
+        }
+    }
+
+    public Transform GetCafeObjectFollowTransform() {
+        return dispenserFrontPoint;
+    }
+
+    public void SetCafeObject(CafeObject cafeObject) {
+        this.cafeObject = cafeObject;
+    }
+
+    public CafeObject GetCafeObject() {
+        return cafeObject;
+    }
+
+    public void ClearCafeObject() {
+        cafeObject = null;
+    }
+
+    public bool HasCafeObject() {
+        return cafeObject != null;
     }
 }
