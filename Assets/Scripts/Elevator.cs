fa3ee9e1042b5397327587944f347e3331abf2cc
refigo <refi@xyzcorp.io>
2024-11-07 16:56:31 +0900

    Test: Try moving elevator roundly

--- Assets/Scripts/Elevator.cs
+++ Assets/Scripts/Elevator.cs
@@ -3,29 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Elevator0 : MonoBehaviour {
-    // [SerializeField] private float moveSpeed = 3f;
-    [SerializeField] Transform firstFloorPosition;
-    [SerializeField] Transform sevenFloorPosition;
+public class Elevator : MonoBehaviour {
+    
+    
+    [SerializeField] private float speed = 20.0f;
+    [SerializeField] private float floorHeight = 10.0f;
+    [SerializeField] private int topFloor = 7;
+    [SerializeField] private int bottomFloor = 1;
+    
+    
+    private int targetFloor;
+    private Vector3 startPosition;
 
-    public void MoveToSeventhFloor() {
-        // Debug.Log("Moving to seventh floor");
-        transform.position = Vector3.Lerp(transform.position, sevenFloorPosition.position, Time.deltaTime);
+
+    void Start() {
+        startPosition = transform.position;
+        targetFloor = topFloor;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    void Update() {
+        Vector3 targetPosition = startPosition + Vector3.up * floorHeight * (targetFloor - 1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        // if (Math.Abs(transform.position - sevenFloorPosition.position) < 0.1f) {
-        //     Debug.Log("Arrived at seventh floor");
-        //     return;
-        // }
-        MoveToSeventhFloor();
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        if (transform.position == targetPosition) {
+            if (targetFloor == topFloor) {
+                targetFloor = bottomFloor;
+            } else if (targetFloor == bottomFloor) {
+                targetFloor = topFloor;
+            }
+        }
     }
 }
